2441e6a3b8680f3f7741eaf57a3270e253e29ec8
Kazhi-Marat <77327653+Kazhi-Marat@users.noreply.github.com>
2021-02-18 10:08:23 +0300

    Add files via upload

---
+++ Untitled1 !.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import math
+a = int(input("Введите число А = "))
+b = int(input("Введите число Б ="))
+y=math.sqrt(a**2+b*22)
+p=y+a+b
+print(p) 
+
+
+# In[5]:
+
+
+a = int(input("Введите число А = "))
+b = int(input("Введите число Б = "))
+с = int(input("Введите число С = "))
+S = a + b
+S = S + c
+print(S)
+
+
+# In[8]:
+
+
+import math
+a = int(input("Введите число А = "))
+b = int(input("Введите число Б = "))
+g = math.sqrt(a*b)
+print(g)
+
+
+# In[1]:
+
+
+import math
+x = int(input("Введите число X = "))
+if x>0:
+    y = x**2
+else:
+    y = math.sqrt(x)
+print(x,y)
+
+
+# In[4]:
+
+
+import math
+a = int(input("Введите число А = "))
+b = int(input("Введите число Б = "))
+c = int(input("Введите число С = "))
+D = b**2-4*a*c
+if D>=0:
+    x1 = ((-b-math.sqrt(D))/2*a)
+    x2 = ((-b+math.sqrt(D))/2*a)
+    print(x1,x2)
+else:
+    print("Решение НЕТ")
+
+
+# x = int(input("Введите число X = "))
+# y = int(input("Введите число Y = "))
+# if x=-0
+#     print("ERROR")
+# else:
+#      z = y/x
+#      print(x,y,z)
+#      
+
+# In[ ]:
+
+
+
+
